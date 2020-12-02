49e44664e996e5b40ce455ced45e3bfe64d0ff77
SathwikaGone <gonesathwika@gmail.com>
2020-12-02 11:39:58 -0500

    tests for actions

--- Utils/index.js
+++ Utils/index.js
@@ -1,4 +1,13 @@
+import { applyMiddleware, createStore } from "redux";
+import rootReducer from "../src/Redux/reducers";
+import { middleware } from "../src/Redux/store";
+
 export const findByTestAt = (component, attr) => {
   const wrapper = component.find(`[data-test='${attr}']`);
   return wrapper;
+};
+
+export const testStore = (initialState) => {
+  const storeMiddlewares = applyMiddleware(...middleware)(createStore);
+  return storeMiddlewares(rootReducer, initialState);
 };
