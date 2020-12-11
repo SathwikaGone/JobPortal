20b1e50b3e1f3e67f3c51a1b77fb507670522c03
SathwikaGone <gonesathwika@gmail.com>
2020-12-11 11:52:41 -0500

    tests in actions

--- src/Redux/actions/course.test.js
+++ src/Redux/actions/course.test.js
@@ -10,4 +10,28 @@ describe("actions", () => {
     };
     expect(actions.add_course(details)).toEqual(expectedAction);
   });
+
+  it("should create an action to get course", () => {
+    let category = "IT";
+    expect(actions.get_course(category)).toEqual({
+      type: types.GET_COURSE,
+      category,
+    });
+  });
+
+  it("should create an action to search by word", () => {
+    let word = "react";
+    expect(actions.get_course_by_search(word)).toEqual({
+      type: types.GET_COURSE_BY_SEARCH,
+      word,
+    });
+  });
+
+  it("should create a action to search by filter", () => {
+    let filter = ["lessthanfive"];
+    expect(actions.get_course_by_filter(filter)).toEqual({
+      type: types.GET_FILTER_DATA,
+      filter,
+    });
+  });
 });
