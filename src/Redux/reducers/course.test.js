f6ada5563c7bcd0b1ad69711e7500c122d2a33af
SathwikaGone <gonesathwika@gmail.com>
2020-12-11 11:53:56 -0500

    reducer default state tests

--- src/Redux/reducers/course.test.js
+++ src/Redux/reducers/course.test.js
@@ -5,29 +5,70 @@ describe("Course Reducer ", () => {
     allCourse: [],
     result: [],
   };
-  it("Should return default state", () => {
-    const newState = courseReducer(undefined, {});
-    expect(newState).toEqual({ ...defaultState });
-  });
 
-  it("Should return default state for initial requests", () => {
-    let data = { result: { data: { addCourse: { name: "Java", cost: 15 } } } };
-    const newState = courseReducer(defaultState, {
-      type: types.CREATE_COURSE,
-      data,
-    });
-    expect(newState).toEqual({ ...defaultState });
-  });
-
-  it("Should return updated state for addCourse success request", () => {
-    let action = {
-      type: types.CREATE_COURSE_SERVER_RESPONSE_SUCCESS,
+  let data;
+  beforeEach(() => {
+    data = {
       result: { data: { addCourse: [{ name: "Java", cost: 15 }] } },
     };
-    const newState = courseReducer(defaultState, action);
-    expect(newState).toEqual({
-      allCourse: [],
-      result: [{ cost: 15, name: "Java" }],
+  });
+
+  describe("Default states", () => {
+    it("Should return default state", () => {
+      const newState = courseReducer(undefined, {});
+      expect(newState).toEqual({ ...defaultState });
+    });
+
+    it("Should return default state for initial CREATE_COURSE requests", () => {
+      const newState = courseReducer(defaultState, {
+        type: types.CREATE_COURSE,
+        data,
+      });
+      expect(newState).toEqual({ ...defaultState });
+    });
+
+    it("Should return default state for initial GET_COURSE requests", () => {
+      let catagory = "IT";
+      const newState = courseReducer(defaultState, {
+        type: types.GET_COURSE,
+        catagory,
+      });
+
+      expect(newState).toEqual({ ...defaultState });
+    });
+
+    it("Should return default state for initial GET_COURSE_BY_SEARCH requests", () => {
+      let word = "react";
+      const newState = courseReducer(defaultState, {
+        type: types.GET_COURSE_BY_SEARCH,
+        word,
+      });
+
+      expect(newState).toEqual({ ...defaultState });
+    });
+
+    it("Should return default state for initial GET_FILTER_DATA requests ", () => {
+      let filter = ["lessthanfive"];
+      const newState = courseReducer(defaultState, {
+        type: types.GET_FILTER_DATA,
+        filter,
+      });
+
+      expect(newState).toEqual({ ...defaultState });
+    });
+  });
+
+  describe("Update states", () => {
+    it("Should return updated state for addCourse success request", () => {
+      let action = {
+        type: types.CREATE_COURSE_SERVER_RESPONSE_SUCCESS,
+        result: data.result,
+      };
+      const newState = courseReducer(defaultState, action);
+      expect(newState).toEqual({
+        allCourse: [],
+        result: [{ cost: 15, name: "Java" }],
+      });
     });
   });
 });
