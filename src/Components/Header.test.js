a7164d4d03675ee1395340b2fd73825cd7e31428
SathwikaGone <gonesathwika@gmail.com>
2020-12-24 16:38:48 -0500

    tests

--- src/Components/Header.test.js
+++ src/Components/Header.test.js
@@ -3,10 +3,9 @@ import { shallow } from "enzyme";
 import Header from "./Header";
 import { findByTestAt, testStore } from "../../Utils/index";
 import * as actions from "../Redux/actions/course";
-
+let mockFunc = jest.fn();
 const setUp = (initialState = {}, props = { dispatch: jest.fn() }) => {
   const store = testStore(initialState);
-  let mockFunc = jest.fn();
 
   const component = shallow(
     <Header store={store} {...props} onClick={mockFunc} />
@@ -34,20 +33,31 @@ describe("Header Component ", () => {
 
   //not working
   it("should trigger a callback event once", () => {
-    mockFunc = jest.fn();
+    // let mockFunc = jest.fn();
     // mockFunc();
     // actions.get_course = jest.fn();
-    const button = findByTestAt(component, "search");
+    // const button = findByTestAt(component, "search");
 
-    console.log("button debug", button.debug());
+    // console.log("button debug", button.debug());
     // button.prop("onClick")();
     // component.instance().searchData(mockFunc);
     // expect(props.dispatch.mock.calls.length).toBe(1);
 
-    button.simulate("click");
+    // button.simulate("click");
 
-    component.find("BsSearch").simulate("click");
-    expect(mockFunc.mock.calls.length).toBe(1);
+    // component.find("BsSearch").simulate("click");
+    // expect(mockFunc.mock.calls.length).toBe(1);
+    // expect(mockFunc).toHaveBeenCalled();
+
+    //https://stackoverflow.com/questions/43747397/simulate-a-button-click-in-jest
+    const store = testStore({});
+    let props = { dispatch: jest.fn() };
+    const component = shallow(
+      <Header store={store} {...props} onClick={mockFunc} />
+    ).dive();
+    const buttonn = findByTestAt(component, "search");
+
+    buttonn.simulate("click");
     expect(mockFunc).toHaveBeenCalled();
   });
 });
